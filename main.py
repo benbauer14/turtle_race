a6c7ace04e5cad3718be6977b1930caf42e27aa3
Ben Bauer <ben.bauer1@gmail.com>
2021-07-15 11:39:50 -0500

    race working with betting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 ben = Turtle("turtle")
 june = Turtle("turtle")
@@ -14,7 +15,7 @@ erica.color("red")
 
 screen = Screen()
 screen.setup(500, 400)
-user_bet = screen.textinput("Pick your horse (turtle)", "Which turtle will win the race? Enter a color:")
+user_bet = screen.textinput("Pick your horse (turtle)", "Which turtle will win the race? Enter a color:").lower()
 ben.penup()
 ben.goto(-200, 150)
 june.penup()
@@ -25,6 +26,43 @@ ray.penup()
 ray.goto(-200, -75)
 erica.penup()
 erica.goto(-200, -150)
+winner = False
+while winner == False:
+    ben.goto(ben.xcor() + random.randint(2, 15), ben.ycor())
+    june.goto(ben.xcor() + random.randint(2, 15), june.ycor())
+    max.goto(ben.xcor() + random.randint(2, 15), max.ycor())
+    ray.goto(ben.xcor() + random.randint(2, 15), ray.ycor())
+    erica.goto(ben.xcor() + random.randint(2, 15), erica.ycor())
+    if(ben.xcor() >= 225):
+        winner = True
+        if(user_bet == 'purple'):
+            print("You win! Purple won the race!")
+        else:
+            print("You lose! Purple won the race!") 
+    elif(june.xcor() >= 225):
+        winner = True
+        if(user_bet == 'blue'):
+            print("You win! Blue won the race!")
+        else:
+            print("You lose! Blue won the race!")  
+    elif(max.xcor() >= 225):
+        winner = True
+        if(user_bet == 'green'):
+            print("You win! Green won the race!")
+        else:
+            print("You lose! Green won the race!")  
+    elif(ray.xcor() >= 225):
+        winner = True
+        if(user_bet == 'orange'):
+            print("You win! Orange won the race!")
+        else:
+            print("You lose! Orange won the race!")  
+    elif(erica.xcor() >= 225):
+        winner = True
+        if(user_bet == 'red'):
+            print("You win! Red won the race!")
+        else:
+            print("You lose! Red won the race!")
 
 
 screen.exitonclick()
